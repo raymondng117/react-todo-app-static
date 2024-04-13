67fc69c5b8b35f71c420503fe3f84ffafbc062d6
RaymondNg <ng000096@algonquinlive.com>
2024-04-12 21:49:22 -0400

    remove all logs

--- src/Components/LoginPage/LogIn.js
+++ src/Components/LoginPage/LogIn.js
@@ -28,7 +28,7 @@ const Login = ({ apiURL }) => {
 
             if (response.ok) {
                 const data = await response.json();
-                if (data.authenticated) {   console.log(data.user);
+                if (data.authenticated) { 
                     sessionStorage.setItem('signedInUser', JSON.stringify(data.user));
                 } else {
                     errorMsg = 'Invalid email or password';
